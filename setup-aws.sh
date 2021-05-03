6233618b431848d7664d523357cd0cab25b3b290
Calleum Pecqueux <calleum.pecqueux@gmail.com>
2021-05-03 09:24:17 +0000

    Update setup-aws.sh

--- setup-aws.sh
+++ setup-aws.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-#gitlab-service-account=`cat gitlab-service-account.yaml`
+#!/bin/bash
 
 set -e
 
@@ -11,7 +9,6 @@ region="${REGION-ap-southeast-2}"
 instance_type="${MACHINE_TYPE-t2.micro}"
 kubeconfig="${KUBECONFIG:-$HOME/.kube/config}"
 gitlab_access_token="${MY_ACCESS_TOKEN-''}"
-#service_account="${SERVICE_ACCOUNT-tiller}"
 
 usage ()
 {
@@ -19,6 +16,32 @@ usage ()
   printf 'Options : \n\t -c cluster-name\n\t -i instance-type\n\t -r region\n\t -t Gitlab Personal Access Token \n\t -n number-of-nodes\n'
   printf 'Default : \n name: gitlab-cluster type: t2.micro region: ap-southeast-2\n\n'
   exit
+}
+
+ post_variable(){
+      local key="${1}"
+      local value="${2}"
+      curl -s --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=${key}" --form "value=${value}" --form "masked=true" --form "protected=true"
+}
+
+ update_variable(){
+      local key="${1}"
+      local value="${2}"
+      curl -s --request PUT --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables/${key}" --form "value=${value}" --form "masked=true" --form "protected=true"
+}
+
+ test_variable(){
+      local key="${1}"
+      local value="${2}"
+      local test
+      test=$(curl -f --request GET --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables/${key}")
+      if [ -z "$test" ]
+      then
+        post_variable "${key}" "${value}"
+      else
+        update_variable "${key}" "${value}"
+      fi
+
 }
 
 while getopts c:n:k:r:i:t:s:h:K opt
@@ -45,9 +68,6 @@ do
     s)
       service_account="${OPTARG}"
       ;;
-    K)
-      kubeconfig="${OPTARG}"
-      ;;
    h)
       usage
       ;;
@@ -70,42 +90,51 @@ then
   exit 1
 fi
     echo "Creating cluster..."
-    eksctl create cluster --name="${cluster_name}" --nodes="${nodes}" --node-type "${instance_type}" --region="${region}"
+    eksctl create cluster --name="${cluster_name}" --nodes="${nodes}" --node-type "${instance_type}" --region="${region}" --asg_access
     echo "Updating cluster config..."
     aws eks update-kubeconfig --name "${cluster_name}"
 
     echo "Updating cluster service account for gitlab..."
     kubectl apply -f gitlab-service-account.yaml
 
-
     SECRET="$(kubectl get secrets | grep default-token| awk '{print $1}')"
     CA="$(kubectl get secret "$SECRET" -o jsonpath="{['data']['ca\.crt']}" | base64 -d)"
 
     echo "Updating gitlab environment variables..."
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=CERTIFICATE_AUTHORITY_DATA" --form "value=${CA}" --form "masked=true" --form "protected=true" |  awk -F \" '/error/ { print $2 }'
+    
+    test_variable "CERTIFICATE_AUTHORITY_DATA" "${CA}"
 
-       ENDPOINT="$(aws eks describe-cluster --region "${region}" --name "${cluster-name}" 2> /dev/null | awk '/endpoint/{print $2}' | sed 's/"//g' | sed 's/,//')"
+    ENDPOINT="$(aws eks describe-cluster --region "${region}" --name "${cluster-name}" 2> /dev/null | awk '/endpoint/{print $2}' | sed 's/"//g' | sed 's/,//')"
 
-
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=SERVER" --form "value=${ENDPOINT}" --form "masked=true" |  awk -F \" '/error/ { print $2 }'
+    test_variable "SERVER" "${ENDPOINT}"
 
     GITLAB_SECRET="$(kubectl get secrets | grep gitlab-service-account-token| awk '{print $1}')"
 
     G_TOKEN="$(kubectl describe secret "$GITLAB_SECRET")"
 
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=USER_TOKEN" --form "value=${G_TOKEN}" --form "masked=true" --form "protected=true" |  awk -F \" '/error/ { print $2 }'
+    test_variable "USER_TOKEN" "${G_TOKEN}"
 
-    KEY_JSON=$(aws iam create-access-key)
+    AWS_ID=$(aws iam get-user | grep ID | awk -F \" '{ print $4 }' )
 
-    AWS_SECRET_ACCESS_KEY=$(echo "$KEY_JSON" | grep -i secret | awk -F \" '{ print $4 }')
+    test_variable "AWS_ID" "${AWS_ID}"
+    
+    if [ -z "${AWS_SECRET_ACCESS_KEY}" ]
+    then
+        KEY_JSON=$(aws iam create-access-key)
 
-    AWS_ACCESS_KEY_ID=$(echo "$KEY_JSON" | grep -i keyid | awk -F \" '{ print $4 }')
+        AWS_SECRET_ACCESS_KEY=$(echo "$KEY_JSON" | grep -i secret | awk -F \" '{ print $4 }')
 
-    AWS_DEFAULT_REGION=${region}
+        AWS_ACCESS_KEY_ID=$(echo "$KEY_JSON" | grep -i keyid | awk -F \" '{ print $4 }')
+
+    fi
+
+    test_variable "AWS_SECRET_KEY" "${AWS_SECRET_KEY}"
+
+    test_variable "AWS_ACCESS_KEY_ID" "${AWS_ACCESS_KEY_ID}"
+
+    test_variable "AWS_DEFAULT_REGION" "${region}"
+
+    test_variable "CLUSTER_NAME" "${cluster_name}"  
 
 
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=AWS_SECRET_ACCESS_KEY" --form "value=${AWS_SECRET_ACCESS_KEY}" --form "masked=true" --form "protected=true" |  awk -F \" '/error/ { print $2 }'
 
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=AWS_ACCESS_KEY_ID" --form "value=${AWS_ACCESS_KEY_ID}" --form "masked=true" --form "protected=true" |  awk -F \" '/error/ { print $2 }'
-
-    curl --request POST --header "PRIVATE-TOKEN: ${MY_ACCESS_TOKEN}" "https://gitlab.com/api/v4/projects/26316511/variables" --form "key=AWS_DEFAULT_REGION" --form "value=${AWS_DEFAULT_REGION}" --form "masked=true" --form "protected=true"|  awk -F \" '/error/ { print $2 }'
